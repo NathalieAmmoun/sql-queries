4a96c6ae2a47f22c5f03eec6bf62cc83d9ed3161
Nathalie Ammoun <78752615+NathalieAmmoun@users.noreply.github.com>
2021-08-24 11:32:08 +0300

    Update sql_queries.sql

--- SQL Queries/sql_queries.sql
+++ SQL Queries/sql_queries.sql
@@ -1,10 +1,10 @@
-SELECT * FROM degrees
+SELECT * FROM degrees;
 
 SELECT F.facFirstName FROM `faculty` F, `degrees` D
-WHERE D.type = 'MS' and D.name = 'Computer Science'
+WHERE D.type = 'MS' and D.name = 'Computer Science';
 
-DELETE FROM `faculty`
+DELETE FROM `faculty`;
 
-UPDATE `faculty` SET `id`='[value-1]',`facFirstName`='[value-2]',`facLastName`='[value-3]',`phone`='[value-4]',`location`='[value-5]',`departemnt`='[value-6]' WHERE 1
+UPDATE `faculty` SET `id`='[value-1]',`facFirstName`='[value-2]',`facLastName`='[value-3]',`phone`='[value-4]',`location`='[value-5]',`departemnt`='[value-6]' WHERE 1;
 
-INSERT INTO `faculty`(`id`, `facFirstName`, `facLastName`, `phone`, `location`, `departemnt`) VALUES ('5','Rania','Safadi','76453122','','Beirut', 'Arts and Sciences')
+INSERT INTO `faculty`(`id`, `facFirstName`, `facLastName`, `phone`, `location`, `departemnt`) VALUES ('5','Rania','Safadi','76453122','Beirut', 'Arts and Sciences');
